7ca6b3536afad08dc5e67c9175308a67d111ebf1
Vasantha Kumar <Vasantha_Thumati@comcast.com>
2016-10-17 16:47:08 -0400

    Removed duplicate file

--- src/main/java/com/ws/crud/operations/exceptions/ServiceExceptionHandler.java
+++ src/main/java/com/ws/crud/operations/exceptions/ServiceExceptionHandler.java
@@ -19,16 +19,16 @@ import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.MissingPathVariableException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @Configurable
 @EnableWebMvc
-@ControllerAdvice
+@RestControllerAdvice
 public class ServiceExceptionHandler extends ResponseEntityExceptionHandler {
 	
 	@ExceptionHandler(Throwable.class)
